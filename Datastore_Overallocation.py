480b984d92cf7684927c4a58ebd72d68d2e27e37
saratlingamarla <saratlingamarla@gmail.com>
2017-05-16 15:22:28 -0400

    Initial Commit

--- Datastore_Overallocation.py
+++ Datastore_Overallocation.py
@@ -6,8 +6,8 @@ def Datastore_Overallocation():
     overallocated_datastore = []
     datastore_list = [datastore for datastore in legacy().get_vim_objects(legacy.content,vim.Datastore)]
     for datastore in datastore_list:
-        if datastore.summary.uncommitted!='None':
-            allocation = ((datastore.summary.capacity - datastore.summary.freeSpace) * 100) / datastore.summary.capacity
+        if datastore.summary.uncommitted!=None:
+            allocation = ((datastore.summary.capacity - datastore.summary.freeSpace+datastore.summary.uncommitted) * 100) / datastore.summary.capacity
             if allocation > 50:
                 overallocated_datastore.append((datastore.summary.name,allocation))
     return overallocated_datastore
